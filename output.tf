f7ba0fbcde959b221f988afdf2cbbfb383d74cfa
Miere Teixeira <miere00@gmail.com>
2019-09-23 12:19:33 +1000

    Exposing log_group_name

--- output.tf
+++ output.tf
@@ -35,3 +35,7 @@ output "route53_record" {
 output "log_group_arn" {
   value = aws_cloudwatch_log_group.default.arn
 }
+
+output "log_group_name" {
+  value = aws_cloudwatch_log_group.default.name
+}
