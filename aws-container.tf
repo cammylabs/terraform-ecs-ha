70def6f649faf20ae938a6c0e98a465b6f2d5ad4
Miere Teixeira <miere00@gmail.com>
2019-12-06 16:25:34 +1100

    Fixing aws-container-task.json

--- aws-container.tf
+++ aws-container.tf
@@ -110,7 +110,7 @@ data "template_file" "container_task" {
     execution_role_arn = aws_iam_role.container.arn
     task_role_arn      = aws_iam_role.container.arn
     datadog_api_key    = var.datadog_api_key
-    app_name           = var.app_name
+    app_name           = var.docker_app_name
     environment        = var.app_environment
   }
 }
